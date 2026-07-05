650c5da4e7bf7ccc7da0c33e8c84acf35cfd5b78
Zaspeh <mdib@fi.uba.ar>
2026-07-05 15:28:21 -0300

    chore: add navigation-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -159,6 +159,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.junit.ktx)
+    implementation(libs.androidx.navigation.compose)
 
     // Unit Testing (Capa de Dominio - src/test)
     testImplementation(libs.junit)
