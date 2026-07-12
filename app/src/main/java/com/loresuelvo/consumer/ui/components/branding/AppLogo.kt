cbac58ec963e21bc234ca427c2216e5e8234a746
Zaspeh <mdib@fi.uba.ar>
2026-07-12 13:52:51 -0300

    Refactor: add scaffold and hero primitives

--- app/src/main/java/com/loresuelvo/consumer/ui/components/branding/AppLogo.kt
+++ app/src/main/java/com/loresuelvo/consumer/ui/components/branding/AppLogo.kt
@@ -7,16 +7,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.loresuelvo.consumer.R
 
 @Composable
-fun AppLogo() {
+fun AppLogo(
+    size: Dp = 96.dp,
+) {
 
     Image(
         painter = painterResource(id = R.drawable.logo),
         contentDescription = stringResource(id = R.string.app_logo_content_description),
         contentScale = ContentScale.Fit,
-        modifier = Modifier.size(96.dp)
+        modifier = Modifier.size(size)
     )
 }
