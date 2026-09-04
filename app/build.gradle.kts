d790a1b68fa7ffb98d090aa34e777f275aea5278
Zaspeh <mdib@fi.uba.ar>
2026-09-04 10:06:49 -0300

    Add[54]: switch compose ui test dep to ui-test-android artifact

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -254,7 +254,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.espresso.intents)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    androidTestImplementation(libs.androidx.compose.ui.test)
+    androidTestImplementation(libs.androidx.compose.ui.test.android)
     // Hilt testing (added in Fase 1)
     androidTestImplementation(libs.hilt.android.testing)
     kaptAndroidTest(libs.hilt.android.compiler)
