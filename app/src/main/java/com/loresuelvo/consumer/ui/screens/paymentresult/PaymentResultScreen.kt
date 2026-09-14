212d06d4eaf38f1f8d33bff00ccda00d9386bbba
Zaspeh <mdib@fi.uba.ar>
2026-09-14 12:14:10 -0300

    Style[21]: When waiting for payment approval, now the screen shows correctly

--- app/src/main/java/com/loresuelvo/consumer/ui/screens/paymentresult/PaymentResultScreen.kt
+++ app/src/main/java/com/loresuelvo/consumer/ui/screens/paymentresult/PaymentResultScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -44,11 +45,11 @@ fun PaymentResultScreen(
 ) {
     Column(
         modifier = modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .padding(horizontal = 24.dp, vertical = 24.dp)
             .testTag(PAYMENT_RESULT_SCREEN_TAG),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.Center,
     ) {
         when (state) {
             is PaymentResultUiState.Loading -> LoadingBlock()
